9ee6f1626464e751dcffbadb8470b72b4dd45c23
ligaab <ligaabola4@gmail.com>
2022-05-18 21:51:37 +0300

    last changes

---
+++ TICTACTOE/TICTACTOE/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+//1 saveicinasanas
+Console.WriteLine("Sveicinats spele TicTacToe");
+
+//2 noteikumi
+Console.WriteLine("Katrs speletajs izvelas apzimejumu X vai O. Pc kartas ")
+//3 laukums 3*3
+Console.WriteLine(" 1 | 2 | 3 |");
+Console.WriteLine("------------");
+Console.WriteLine(" 4 | 5 | 6 |");
+Console.WriteLine("------------");
+Console.WriteLine(" 7 | 8 | 9 |");
+
+//4 speletaju gajienu izvele x vai o
+Console.WriteLine("Ludzu izvelies simbolu X vai O?");
+Console.ReadLine();
+//5 laukuma definesana masivaa lidz 9
+//6 nelaut atkartoti aizpildit, jau izveletu laucinu
+//7 uzvaretaja noteiksana
+//8 kas notiek ja laucini aizpilditi, bet nav uzvaretaja (restartet speli un spelet velreiz)
+//9 noteikt vai speletajs ir atzimejis laucinu ar x vai o
+
