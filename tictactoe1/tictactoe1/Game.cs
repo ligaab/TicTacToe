5e59eefa63a4109607682ec83ab7075ee5236836
ligaab <ligaabola4@gmail.com>
2022-05-25 21:43:59 +0300

    last changes

---
+++ tictactoe1/tictactoe1/Game.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace tictactoe1
+{
+    public class Game
+    {
+        public static string[] pos = new string[10] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
+
+       public static void Main(string[] args)
+        {
+            //DrawBoard();
+            //Console.WriteLine("");
+        }
+            public static void DrawBoard()
+        {
+            Console.WriteLine("   {0}  |  {1}  |  {2}   ", pos[1], pos[2], pos[3]);
+            Console.WriteLine("-------------------");
+            Console.WriteLine("   {0}  |  {1}  |  {2}   ", pos[4], pos[5], pos[6]);
+            Console.WriteLine("-------------------");
+            Console.WriteLine("   {0}  |  {1}  |  {2}   ", pos[7], pos[8], pos[9]);
+
+            
+        }
+
+           
+        
+    }
+}
---
+++ tictactoe1/tictactoe1/Program.cs
@@ -0,0 +1,24 @@
+﻿
+
+
+using static tictactoe1.Game;
+
+
+    Console.WriteLine("Laipni lugts spele TicTacToe");
+    Console.WriteLine("Katram speletajam ir simbols X vai O. Uzvar tas kurs pirmais savac tris vienadus simbolus rinda, kolonna, pa diognali");
+    Console.WriteLine();
+    Console.WriteLine();
+
+    string player1 = "X";
+    string player2 = "O";
+    Console.WriteLine($"Speletajs 1 ir {player1}, speletajs 2 ir {player2}");
+
+DrawBoard();
+  Console.WriteLine("");
+
+
+
+
+
+
+
