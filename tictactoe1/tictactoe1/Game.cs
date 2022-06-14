ce1df1474fb234191b832b656be35663c1eabae5
Gausins <andris.gausins@gmail.com>
2022-06-14 17:25:05 +0300

    X vai O Win

--- tictactoe1/tictactoe1/Game.cs
+++ tictactoe1/tictactoe1/Game.cs
@@ -11,15 +11,16 @@ namespace tictactoe1
         public static int Player = 1;
         public static int step;
         public static int flag = 0;
+        /*public static bool input = false*/
 
         public static string[] pos = new string[10] { "0", "1", "2", "3", "4", "5", "6", "7", "8", "9" };
 
-       public static void Main(string[] args)
-        {
-            //DrawBoard();
-            //Console.WriteLine("");
-        }
-            public static void DrawBoard()
+        //public static void Main(string[] args)
+        //{
+        //    //DrawBoard();
+        //    //Console.WriteLine("");
+        //}
+        public static void DrawBoard()
         {
             Console.WriteLine(GetFirstLineTekst(pos));
             Console.WriteLine("-------------------");
@@ -27,13 +28,13 @@ namespace tictactoe1
             Console.WriteLine("-------------------");
             Console.WriteLine(GetThirdLineTekst(pos));
 
-            
+
         }
         public static string GetFirstLineTekst(string[] values)
         {
             string text = $"  {values[1]}  |  {values[2]}  |  {values[3]}  ";
             return text;
-            
+
         }
         public static string GetSecondLineTekst(string[] values)
         {
@@ -47,7 +48,19 @@ namespace tictactoe1
             return text;
 
         }
+        //public static bool correctInput()
+        //{
+        //    if (pos[step] == "X" && pos[step] == "O") 
+        //    {
+        //        return true;
+        //    }
 
+        //    else
+        //    { 
+        //        return false;
+        //    }
+        //}
+    
         public static int CheckWin()
         {
             if (pos[1] == "O" && pos[2] == "O" && pos[3] == "O" ||
@@ -57,22 +70,28 @@ namespace tictactoe1
                    pos[7] == "O" && pos[5] == "O" && pos[3] == "O" ||
                    pos[1] == "O" && pos[4] == "O" && pos[7] == "O" ||
                    pos[2] == "O" && pos[5] == "O" && pos[8] == "O" ||
-                   pos[3] == "O" && pos[6] == "O" && pos[9] == "O" ||
-                   pos[1] == "X" && pos[2] == "X" && pos[3] == "X" ||
-                   pos[4] == "X" && pos[5] == "X" && pos[6] == "X" ||
-                   pos[7] == "X" && pos[8] == "X" && pos[9] == "X" ||
-                   pos[1] == "X" && pos[5] == "X" && pos[9] == "X" ||
-                   pos[7] == "X" && pos[5] == "X" && pos[3] == "X" ||
-                   pos[1] == "X" && pos[4] == "X" && pos[7] == "X" ||
-                   pos[2] == "X" && pos[5] == "X" && pos[8] == "X" ||
-                   pos[3] == "X" && pos[6] == "X" && pos[9] == "X")
+                   pos[3] == "O" && pos[6] == "O" && pos[9] == "O")
             {
-                Console.WriteLine("1");
+                //Console.WriteLine("1");
                 return 1;
+            }
+           
+            else if
+                  (pos[1] == "X" && pos[2] == "X" && pos[3] == "X" ||
+                  pos[4] == "X" && pos[5] == "X" && pos[6] == "X" ||
+                  pos[7] == "X" && pos[8] == "X" && pos[9] == "X" ||
+                  pos[1] == "X" && pos[5] == "X" && pos[9] == "X" ||
+                  pos[7] == "X" && pos[5] == "X" && pos[3] == "X" ||
+                  pos[1] == "X" && pos[4] == "X" && pos[7] == "X" ||
+                  pos[2] == "X" && pos[5] == "X" && pos[8] == "X" ||
+                  pos[3] == "X" && pos[6] == "X" && pos[9] == "X")
+            {
+                //Console.WriteLine("2");
+                return 2;
             }
             else if (pos[1] != "1" && pos[2] != "2" && pos[3] != "3" && pos[4] != "4" && pos[5] != "5" && pos[6] != "6" && pos[7] != "7" && pos[8] != "8" && pos[9] != "9")
             {
-                Console.WriteLine("-1");
+                //Console.WriteLine("-1");
                 return -1;
             }
             else
@@ -80,11 +99,15 @@ namespace tictactoe1
                 Console.WriteLine("0");
                 return 0;
             }
-
         }
-    }
-
-
+       
 
     }
+}
+
+
+
+
+
+    
 
--- tictactoe1/tictactoe1/Program.cs
+++ tictactoe1/tictactoe1/Program.cs
@@ -19,51 +19,66 @@ Console.WriteLine($"Spēlētājs 2 ir {charValue2}");
     Console.WriteLine();
     Console.WriteLine();
 
-
-
-
-
-
+DrawBoard();
 
 do
 {
-   
+
     Console.WriteLine("speletaja 1 gajiens");
     step = int.Parse(Console.ReadLine());
     pos[step] = "X";
-    bool correctInput = true;
+    
     Console.Clear();
     DrawBoard();
-
+   
     flag = CheckWin();
-    
+    //input = correctInput();
+   
+
 
     Console.WriteLine("speletaja 2 gajiens");
-    step = int.Parse(Console.ReadLine());
-    pos[step] = "O";
-    bool correctInput1 = true;
-    Console.Clear();
-    DrawBoard();
-
-    flag = CheckWin();
+        step = int.Parse(Console.ReadLine());
+        pos[step] = "O";
+        
+        Console.Clear();
+        DrawBoard();
     
+    flag = CheckWin();
+    //input = correctInput();
+      
 }
-while (flag !=1 && flag != -1);
+while (flag != 1 && flag != -1 && flag != 2);
 
-if (flag == 1)
-{
-    Console.WriteLine($"Player  {charValue} has von");
+    if (flag == 1)
+    {
+        Console.WriteLine($"Player  {charValue2} has von");
 
-}
-else if (flag == 0)
-{
-    Console.WriteLine("player has von");
+    }
+    else if (flag == 2)
+    {
 
-}
-else
-{
-    Console.WriteLine("DrawBoard");
-}
+        Console.WriteLine($"Player  {charValue} has von");
+
+    }
+    else
+    {
+        flag = 0;
+    }
+
+
+//if (input == true)
+//{
+//    Console.WriteLine("Izvēlies citu skaitli!");
+//}
+//else
+//{
+//    Console.WriteLine("Turpini spēli!");
+//}
+
+
+
+
+
 
 
 // jāizveido pārbaudes katrai rindai ar uzvaru
