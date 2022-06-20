4d6324919733b43f770879ac300162230ca54d9d
Gausins <andris.gausins@gmail.com>
2022-06-20 21:38:53 +0300

    Aizņemtu lauciņu kontrole - ir Jāpabeidz spēles nobeigums1

--- tictactoe1/tictactoe1/Program.cs
+++ tictactoe1/tictactoe1/Program.cs
@@ -1,11 +1,6 @@
-﻿
-
-
-using tictactoe1;
+﻿using tictactoe1;
 using static tictactoe1.Game;
 using static tictactoe1.Simbol;
-
-
 
 
 Console.WriteLine("Laipni lugts spele TicTacToe");
@@ -20,102 +15,123 @@ player2.GetPlayerName();
 player2.Simbol = Simbol.O;
 
 
-//tictactoe1.Player diferntPlayer = tictactoe1.Player.Player1;
-
 //char charValue = (char)Player1;
 Console.WriteLine($"Spēlētājs 1 ir {player1.Simbol}, tavs vards ir {player1.Name}");
 
 //char charValue2 = (char)Player2;
 Console.WriteLine($"Spēlētājs 2 ir {player2.Simbol}, tavs vards ir {player2.Name}");
 
-
     Console.WriteLine("Katram speletajam ir simbols X vai O. Uzvar tas kurs pirmais savac tris vienadus simbolus rinda, kolonna, pa diognali");
     Console.WriteLine();
     Console.WriteLine();
-
-
 DrawBoard();
 
 do
-{
-
+{   
     Console.WriteLine("speletaja 1 gajiens");
     step1 = int.Parse(Console.ReadLine());
     pos[step1] = "X";
     Console.Clear();
     DrawBoard();
-    
     flag = Game.CheckWin();
     board = boardFull();
-
-    if (Game.flag == 1)
-
-    {
-        break;
-    }
     input = Game.correctInput();
-    if (input == false)
-    {
-        Console.WriteLine("izvēlies citu skaitli!");
-        Console.ReadLine();
-    }
-    else
-    {
-        Console.WriteLine("");
-    }
-    
 
+    {
+        if (Game.flag == 1)
+        {
+            break;
+        }
+       
+        else if (input == false)
+        {
+            Console.Clear();
+            DrawBoard();
+            Console.WriteLine("izvēlies citu skaitli!");
+            step1 = int.Parse(Console.ReadLine());            
+        }
+        else
+        {
+            Console.WriteLine("");
+        }
+        if (board == -1)
+        {
+            Console.WriteLine("Viss laukums ir aizpildīts - NEZŠĶIRTS REZULTĀTS!");
+            break;
+        }
+    }
+   
 
     Console.WriteLine("speletaja 2 gajiens");
     step2 = int.Parse(Console.ReadLine());
     pos[step2] = "O";
     Console.Clear();
     DrawBoard();
-
     flag = Game.CheckWin();
     board = boardFull();
-
-    if (Game.CheckWin() == 2)
+    input = Game.correctInput();
 
     {
+        if (Game.CheckWin() == 2)
+        {
+            break;
+        }
+        
+        else if (input == false)
+        {
+            Console.Clear();
+            DrawBoard();
+            Console.WriteLine("izvēlies citu skaitli!");
+            step2 = int.Parse(Console.ReadLine());         
+        }
+        else
+        {
+            Console.WriteLine("");
+        }
+    }
+    if (board == -1)
+    {
+        Console.WriteLine("Viss laukums ir aizpildīts - NEZŠĶIRTS REZULTĀTS!");
         break;
     }
-    input = Game.correctInput();
-    if (input == false)
-    {
-        Console.WriteLine("izvēlies citu skaitli!");
-        Console.ReadLine();
-    }
-    else
-    {
-        Console.WriteLine("");
-    }
-   }
+    
+}
 while (flag == 0);
-
-
+{
     if (flag == 2)
     {
-        Console.WriteLine($"Player  {player2.Name} has von");
-        Console.WriteLine("Spēlējam vēlreiz?");
-
-        DrawBoard();
-
+        Console.WriteLine($"Spēlētājs  {player2.Name} ir uzvarējis");      
     }
     else if (flag == 1)
     {
-
-        Console.WriteLine($"Player  {player1.Name} has von");
-        Console.WriteLine("Spēlējam vēlreiz?");
-
-
+        Console.WriteLine($"Spēlētājs  {player1.Name} ir uzvarējis");     
     }
+}
+if (flag == 2 || flag == 1 || board == -1)
+{
+    Console.WriteLine("Ko darīsim tālāk?");
+    Console.WriteLine("1. Spēlēt atkal");// pagaidām vēl nestrādā!
+    Console.WriteLine("2. Pamest spēli");
+    Console.WriteLine("");
+    Console.WriteLine("Ievadi savu izvēli: ");
+    choice = int.Parse(Console.ReadLine());
 
-    else if (Game.boardFull() != -1)
+    if (choice == 1)
     {
-        Console.WriteLine("Viss laukums ir aizpildīts - NEZŠĶIRTS REZULTĀTS!");
+        Console.Clear();
+        DrawBoard();
+        
+    }
+    else if (choice == 2)
+    {
+        Console.Clear();
+        Console.WriteLine("Paldies par Spēli!");
 
     }
+}
+
+
+
 
 
 
